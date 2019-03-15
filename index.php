15dff16bcc72d74d2b5af5dfaf4015ff215c4046
tenglei.ning <tenglei.ning@xianghuanji.com>
2019-03-15 14:57:54 +0800

    code v1

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
-class Sms{
+class Sms
+{
 
-  private function _do_request($url, $params, $is_post = false, $headers = [])
+    private function _do_request($url, $params, $is_post = false, $headers = [])
     {
         if (!$is_post) {
             if ($params) {
@@ -22,10 +23,9 @@ class Sms{
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-	    if(!empty($headers) && is_array($headers))
-	    {
-	        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	    }
+        if (!empty($headers) && is_array($headers)) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        }
 
         if ($is_post) {
             curl_setopt($ch, CURLOPT_POST, true);
@@ -44,122 +44,124 @@ class Sms{
     }
 
     //借帮帮
-    public function jiebangbang($phone){
-    	$params = [
-    		'phoneNumber' => $phone,
-    		'channelCode' => 'aGse3e',
-    		'signTemplateId' => 'jbb',
-    	];
-    	$url = 'https://api.jiebangbang.cn/manager/mgt/msgCode';
-    	return $this->_do_request($url, $params);
+    public function jiebangbang($phone)
+    {
+        $params = [
+            'phoneNumber' => $phone,
+            'channelCode' => 'aGse3e',
+            'signTemplateId' => 'jbb',
+        ];
+        $url = 'https://api.jiebangbang.cn/manager/mgt/msgCode';
+        return $this->_do_request($url, $params);
     }
 
     // 易贷钱庄
-    public function jiugeji($phone) {
-    	$params = [
-    		'phone' => $phone,
-    	];
+    public function jiugeji($phone)
+    {
+        $params = [
+            'phone' => $phone,
+        ];
 
-    	$url = "https://www.jiugeji.com/www/send-sms";
-    	return $this->_do_request($url, $params, true);
+        $url = "https://www.jiugeji.com/www/send-sms";
+        return $this->_do_request($url, $params, true);
     }
 
-    public function jiedianqian($phone) {
-    	$params=[
-    		'isLoading' => 'false',
-    		'deviceType' => 'MSite',
-    		'tokenID' => '',
-    		'mobile' => $phone,
-    		'captchaCode' => '',
-    		'codeLength' => '4'
-    	];
-    	$url = 'https://m.jiedianqian.com/verify/account/send_verify_code.do';
-    	return $this->_do_request($url, $params);
+    public function jiedianqian($phone)
+    {
+        $params = [
+            'isLoading' => 'false',
+            'deviceType' => 'MSite',
+            'tokenID' => '',
+            'mobile' => $phone,
+            'captchaCode' => '',
+            'codeLength' => '4'
+        ];
+        $url = 'https://m.jiedianqian.com/verify/account/send_verify_code.do';
+        return $this->_do_request($url, $params);
     }
 
-	public function getHtml($url, $httpHeader = [], $get_cookie = false){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    public function getHtml($url, $httpHeader = [], $get_cookie = false)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
-	    curl_setopt($ch, CURLOPT_HEADERFUNCTION, function ($ch, $str) use(&$setcookie) {
-	      // 第一个参数是curl资源，第二个参数是每一行独立的header!
-	    list ($name, $value) = array_map('trim', explode(':', $str, 2));
-			$name = strtolower($name);
-			if('set-cookie'==$name)
-			{
-				$setcookie[]=$value;
-			}
-			return strlen($str);
-	    });
+        curl_setopt($ch, CURLOPT_HEADERFUNCTION, function ($ch, $str) use (&$setcookie) {
+            // 第一个参数是curl资源，第二个参数是每一行独立的header!
+            list ($name, $value) = array_map('trim', explode(':', $str, 2));
+            $name = strtolower($name);
+            if ('set-cookie' == $name) {
+                $setcookie[] = $value;
+            }
+            return strlen($str);
+        });
 
-		$html = curl_exec($ch);
-		curl_close($ch);
+        $html = curl_exec($ch);
+        curl_close($ch);
 
-		$cookie = array();
-	    foreach($setcookie as $c)
-	    {
-	    	$tmp = explode(";",$c);
-	    	$cookie[] = $tmp[0];
-	    }
-	    $cookiestr = "Cookie: ".implode(";", $cookie);
+        $cookie = array();
+        foreach ($setcookie as $c) {
+            $tmp = explode(";", $c);
+            $cookie[] = $tmp[0];
+        }
+        $cookiestr = "Cookie: " . implode(";", $cookie);
 
-	    if ($get_cookie) {
-	    	return [$html, $cookiestr];
-	    }else{
-	    	$html;
-	    }
-	}
-
-	public function getCookie($url){
-	    $ch = curl_init();
-	    curl_setopt($ch, CURLOPT_URL, $url);
-	    curl_setopt($ch, CURLOPT_NOBODY, true);
-	    curl_setopt($ch, CURLOPT_HEADERFUNCTION, function ($ch, $str) use(&$setcookie) {
-	      // 第一个参数是curl资源，第二个参数是每一行独立的header!
-	    list ($name, $value) = array_map('trim', explode(':', $str, 2));
-			$name = strtolower($name);
-			if('set-cookie'==$name)
-			{
-				$setcookie[]=$value;
-			}
-			return strlen($str);
-	    });
-	    curl_exec($ch);
-	    curl_close($ch);
-	    $cookie = array();
-	    foreach($setcookie as $c)
-	    {
-	    	$tmp = explode(";",$c);
-	    	$cookie[] = $tmp[0];
-	    }
-	    $cookiestr = "Cookie:".implode(";", $cookie);
-	    return $cookiestr;
+        if ($get_cookie) {
+            return [$html, $cookiestr];
+        } else {
+            $html;
+        }
     }
 
-
-    public function suzhouzyc($phone){
-    	$params=[
-    		'channel' => "kqsxd09",
-    		'client' => 'kuaiqd-1.0.0-ios-1.0.0',
-    		'mobile' => $phone,
-    		'type'=> "reg"
-    	];
-
-    	$url = 'https://kqd.suzhouzyc.cn/v2/user/smscode';
-    	return $this->_do_request($url, $params, true);
+    public function getCookie($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_NOBODY, true);
+        curl_setopt($ch, CURLOPT_HEADERFUNCTION, function ($ch, $str) use (&$setcookie) {
+            // 第一个参数是curl资源，第二个参数是每一行独立的header!
+            list ($name, $value) = array_map('trim', explode(':', $str, 2));
+            $name = strtolower($name);
+            if ('set-cookie' == $name) {
+                $setcookie[] = $value;
+            }
+            return strlen($str);
+        });
+        curl_exec($ch);
+        curl_close($ch);
+        $cookie = array();
+        foreach ($setcookie as $c) {
+            $tmp = explode(";", $c);
+            $cookie[] = $tmp[0];
+        }
+        $cookiestr = "Cookie:" . implode(";", $cookie);
+        return $cookiestr;
     }
 
 
+    public function suzhouzyc($phone)
+    {
+        $params = [
+            'channel' => "kqsxd09",
+            'client' => 'kuaiqd-1.0.0-ios-1.0.0',
+            'mobile' => $phone,
+            'type' => "reg"
+        ];
 
-    public function JinXiangHui($phone) {
-    	 $headers = [
-        	'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
-        	'Referer: http://m.jinxianghui.net/?source=248',
-        	'X-Requested-With: XMLHttpRequest',
-        	'Origin: http://m.jinxianghui.net',
-        	'Host: m.jinxianghui.net',
-        	'Content-Type: application/x-www-form-urlencoded; charset=UTF-8'
+        $url = 'https://kqd.suzhouzyc.cn/v2/user/smscode';
+        return $this->_do_request($url, $params, true);
+    }
+
+
+    public function JinXiangHui($phone)
+    {
+        $headers = [
+            'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
+            'Referer: http://m.jinxianghui.net/?source=248',
+            'X-Requested-With: XMLHttpRequest',
+            'Origin: http://m.jinxianghui.net',
+            'Host: m.jinxianghui.net',
+            'Content-Type: application/x-www-form-urlencoded; charset=UTF-8'
         ];
 
         $html = $this->getHtml('http://m.jinxianghui.net/?source=248', $headers, true);
@@ -168,48 +170,85 @@ class Sms{
         preg_match_all('/crsf\:\ \'([\s\S]*?)\'\,/', $html[0], $crsf);
         preg_match_all('/uuid\:\ \'([\s\S]*?)\'\,/', $html[0], $uuid);
 
-    	$params = [
-    		'_crsf' => $crsf[1][1] ?? '',
-    		'phone' => $phone,
-    		'uuid' => $uuid[1][0] ?? '',
-    		'source'=> "248",
-    	];
-    	$url = 'http://m.jinxianghui.net/page/send';
-    	return $this->_do_request($url, $params, true, $headers);
+        $params = [
+            '_crsf' => $crsf[1][1] ?? '',
+            'phone' => $phone,
+            'uuid' => $uuid[1][0] ?? '',
+            'source' => "248",
+        ];
+        $url = 'http://m.jinxianghui.net/page/send';
+        return $this->_do_request($url, $params, true, $headers);
     }
 
-    public function send($phone){
-    	$rs = $this->jiebangbang($phone);
-    	if ($rs['resultCode'] == '0') { 
-    		echo "JieBangBang send success\n";
-    	}else{
-    		echo "JieBangBang send Error\n";
-    	}
+    public function HouPutech($phone) {
+        $params = [
+            'm' => $phone,
+            'source' => "jlm",
+            'c' => "ea484b7edff64accb2a37a02804864b0"
+        ];
 
-    	$rs = $this->jiugeji($phone);
-    	if ($rs == 'true') {
-    		echo "JiuGeJi send success\n";
-    	}else{
-    		echo "JiuGeJi send Error\n";
-    	}
+        $url = 'https://h5.houputech.com/LoanMarketJLM/PromotionSendSmsUnVerify';
+        return $this->_do_request($url, $params, true); 
+    }
 
-    	$rs = $this->jiedianqian($phone);
+    public function ArticleCard($phone){
+        $params = [
+            'phoneNum' => $phone,
+        ];
+        $url = 'https://lm.articlecard.com/app/user/sms_code_loan';
 
-    	$rs = $this->suzhouzyc($phone);
+        return $this->_do_request($url, $params, true); 
+    }
 
-    	if (isset($rs['c']) && $rs['c'] == '200') {
-    		echo "SuZhouZyc send success\n";
-    	}else{
-    		echo "SuZhouZyc send Error\n";
-    	}
+    public function send($phone)
+    {
+        $rs = $this->jiebangbang($phone);
+        if ($rs['resultCode'] == '0') {
+            echo "JieBangBang send success\n";
+        } else {
+            echo "JieBangBang send Error\n";
+        }
 
-    	$rs = $this->JinXiangHui($phone);
+        $rs = $this->jiugeji($phone);
+        if ($rs == 'true') {
+            echo "JiuGeJi send success\n";
+        } else {
+            echo "JiuGeJi send Error\n";
+        }
 
-    	if (isset($rs['code']) && $rs['code'] == '200') {
-    		echo "JinXiangHui send success\n";
-    	}else{
-    		echo "JinXiangHui send Error\n";
-    	}
+        $rs = $this->jiedianqian($phone);
+
+        $rs = $this->HouPutech($phone);
+
+        if (isset($rs['Status']) && $rs['Status'] == '1') {
+            echo "HouPutech send success\n";
+        } else {
+            echo "HouPutech send Error\n";
+        }
+
+        $rs = $this->ArticleCard($phone);
+
+        if (isset($rs['errcode']) && $rs['errcode'] == '0') {
+            echo "ArticleCard send success\n";
+        } else {
+            echo "ArticleCard send Error\n";
+        }
+
+        $rs = $this->suzhouzyc($phone);
+
+        if (isset($rs['c']) && $rs['c'] == '200') {
+            echo "SuZhouZyc send success\n";
+        } else {
+            echo "SuZhouZyc send Error\n";
+        }
+
+        $rs = $this->JinXiangHui($phone);
+
+        if (isset($rs['code']) && $rs['code'] == '200') {
+            echo "JinXiangHui send success\n";
+        } else {
+            echo "JinXiangHui send Error\n";
+        }
     }
 }
 
